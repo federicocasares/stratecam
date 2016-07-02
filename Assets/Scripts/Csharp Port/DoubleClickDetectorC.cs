40ba9359fbdcf18940286e2d22e053537a4f3d99
Sven Geusens <sven@roodfluweel.be>
2016-07-02 17:04:38 +0200

    Ported StrateCam to C#. Added some transform extensions to get around the vector3 struct problem.

---
+++ Assets/Scripts/Csharp Port/DoubleClickDetectorC.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoubleClickDetectorC : MonoBehaviour {
+
+    private int numberOfClicks = 0;
+	private float timer = 0.0f;
+	
+	public bool IsDoubleClick()
+    {
+        var isDoubleClick = numberOfClicks == 2;
+        if (isDoubleClick)
+            numberOfClicks = 0;
+        return isDoubleClick;
+    }
+
+    public void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 0.3f)
+        {
+            numberOfClicks = 0;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            numberOfClicks++;
+            timer = 0.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Csharp Port/TransformExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UnityEngine.Extensions
+{
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Updates the local eulerAngles to a new vector3, if a value is omitted then the old value will be used.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public static void SetLocalEulerAngles(this Transform transform, float? x = null, float? y = null, float? z = null)
+        {
+            var newVector = new Vector3();
+            var oldVector = transform.localEulerAngles;
+            newVector.x = x != null ? x.Value : oldVector.x;
+            newVector.y = y != null ? y.Value : oldVector.y;
+            newVector.z = z != null ? z.Value : oldVector.z;
+            transform.localEulerAngles = newVector;
+        }
+
+        /// <summary>
+        /// Updates the position to a new vector3, if a value is omitted then the old value will be used.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public static void SetPosition(this Transform transform, float? x = null, float? y = null, float? z = null)
+        {
+            var newVector = new Vector3();
+            var oldVector = transform.position;
+            newVector.x = x != null ? x.Value : oldVector.x;
+            newVector.y = y != null ? y.Value : oldVector.y;
+            newVector.z = z != null ? z.Value : oldVector.z;
+            transform.position = newVector;
+        }
+    }
+}
